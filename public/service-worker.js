538ab266a5618b5576b95653fe6a26d68b586d54
HRush <hrkoren@gmail.com>
2021-05-22 16:24:00 -0400

    added screenshot of application; working on storing cache for offline mode

--- public/service-worker.js
+++ public/service-worker.js
@@ -12,31 +12,28 @@ const FILES_TO_CACHE = [
     "/icons/icon-512x512.png",
 ];
 
-// install
+// install data cache
 self.addEventListener("install", function (evt) {
-    // pre cache image data
     evt.waitUntil(
-        caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/images"))
+        caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api"))
     );
-
-    // pre cache all static assets
+    // pre cache static assets
     evt.waitUntil(
         caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
     );
 
-    // tell the browser to activate this service worker immediately once it
-    // has finished installing
+    //activate service worker once installed
     self.skipWaiting();
 });
 
-// activate
+// activate cache
 self.addEventListener("activate", function (evt) {
     evt.waitUntil(
         caches.keys().then(keyList => {
             return Promise.all(
                 keyList.map(key => {
                     if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-                        console.log("Removing old cache data", key);
+                        console.log("Remove old cache data", key);
                         return caches.delete(key);
                     }
                 })
@@ -47,27 +44,23 @@ self.addEventListener("activate", function (evt) {
     self.clients.claim();
 });
 
-// fetch
+// fetch cache and store 
 self.addEventListener("fetch", function (evt) {
     if (evt.request.url.includes("/api/")) {
         evt.respondWith(
             caches.open(DATA_CACHE_NAME).then(cache => {
                 return fetch(evt.request)
                     .then(response => {
-                        // If the response was good, clone it and store it in the cache.
                         if (response.status === 200) {
                             cache.put(evt.request.url, response.clone());
                         }
-
                         return response;
                     })
                     .catch(err => {
-                        // Network request failed, try to get it from the cache.
                         return cache.match(evt.request);
                     });
             }).catch(err => console.log(err))
         );
-
         return;
     }
 
