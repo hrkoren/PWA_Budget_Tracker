9415801b5cf14bb276ea9d1fb7a72d4296708747
HRush <hrkoren@gmail.com>
2021-05-25 21:52:57 -0400

    working on code in service worker for cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "static-cache-v2";
-const DATA_CACHE_NAME = "data-cache-v1";
+const CACHE_NAME = 'static-cache-v2';
+const DATA_CACHE_NAME = 'data-cache-v1';
 
 const FILES_TO_CACHE = [
     "/",
@@ -13,62 +13,39 @@ const FILES_TO_CACHE = [
 ];
 
 // install data cache
-self.addEventListener("install", function (evt) {
-    evt.waitUntil(
-        caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api"))
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches
+            .open(DATA_CACHE_NAME)
+            .then((cache) => cache.add(FILES_TO_CACHE))
     );
-    // pre cache static assets
-    evt.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
-    );
-
-    //activate service worker once installed
     self.skipWaiting();
 });
 
-// activate cache
-self.addEventListener("activate", function (evt) {
-    evt.waitUntil(
-        caches.keys().then(keyList => {
-            return Promise.all(
-                keyList.map(key => {
-                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-                        console.log("Remove old cache data", key);
-                        return caches.delete(key);
-                    }
-                })
-            );
-        })
-    );
+// start the cache
+// self.addEventListener('activate', (event) => {
+//     event.waitUntil(
+//         caches
+//             .keys()
+//             .then(keyList => {
+//                 return Promise.all(
+//                     keyList.map(key => {
+//                         if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+//                             console.log('Remove old data', key);
+//                             return caches.delete(key);
+//                         }
+//                     })
+//                 );
+//             })
+//     );
+//     self.clients.claim();
+// });
 
-    self.clients.claim();
-});
-
-// fetch cache and store 
-self.addEventListener("fetch", function (evt) {
-    if (evt.request.url.includes("/api/")) {
-        evt.respondWith(
-            caches.open(DATA_CACHE_NAME).then(cache => {
-                return fetch(evt.request)
-                    .then(response => {
-                        if (response.status === 200) {
-                            cache.put(evt.request.url, response.clone());
-                        }
-                        return response;
-                    })
-                    .catch(err => {
-                        return cache.match(evt.request);
-                    });
-            }).catch(err => console.log(err))
-        );
-        return;
-    }
-
-    evt.respondWith(
-        caches.open(CACHE_NAME).then(cache => {
-            return cache.match(evt.request).then(response => {
-                return response || fetch(evt.request);
-            });
+// fetch the cache
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
         })
     );
 });
