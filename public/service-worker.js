ab4d24a6995dbd0f7165d37b6ee78125657f1578
HRush <hrkoren@gmail.com>
2021-05-26 17:09:39 -0400

    working on caching; cleaned up errors in console

--- public/service-worker.js
+++ public/service-worker.js
@@ -13,44 +13,44 @@ const FILES_TO_CACHE = [
 ];
 
 // install data cache
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches
-            .open(DATA_CACHE_NAME)
-            .then((cache) => cache.add(FILES_TO_CACHE))
-    );
-    event.waitUntil(
-        caches
-            .open(CACHE_NAME)
-            .then((cache) => cache.addAll(FILES_TO_CACHE))
-    );
-    self.skipWaiting();
-});
+// self.addEventListener('install', (event) => {
+//     event.waitUntil(
+//         caches
+//             .open(DATA_CACHE_NAME)
+//             .then((cache) => cache.add(FILES_TO_CACHE))
+//     );
+//     event.waitUntil(
+//         caches
+//             .open(CACHE_NAME)
+//             .then((cache) => cache.addAll(FILES_TO_CACHE))
+//     );
+//     self.skipWaiting();
+// });
 
-// start the cache
-self.addEventListener('activate', (event) => {
-    event.waitUntil(
-        caches
-            .keys()
-            .then(keyList => {
-                return Promise.all(
-                    keyList.map(key => {
-                        if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-                            console.log('Remove old data', key);
-                            return caches.delete(key);
-                        }
-                    })
-                );
-            })
-    );
-    self.clients.claim();
-});
+// // start the cache
+// self.addEventListener('activate', (event) => {
+//     event.waitUntil(
+//         caches
+//             .keys()
+//             .then(keyList => {
+//                 return Promise.all(
+//                     keyList.map(key => {
+//                         if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+//                             console.log('Remove old data', key);
+//                             return caches.delete(key);
+//                         }
+//                     })
+//                 );
+//             })
+//     );
+//     self.clients.claim();
+// });
 
-// fetch the cache
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
-    );
-});
+// // fetch the cache
+// self.addEventListener('fetch', (event) => {
+//     event.respondWith(
+//         caches.match(event.request).then(response => {
+//             return response || fetch(event.request);
+//         })
+//     );
+// });
