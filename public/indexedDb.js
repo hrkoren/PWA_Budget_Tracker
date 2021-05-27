4b8fa190c145c7bc6b5cc7ce29ef181703696577
HRush <hrkoren@gmail.com>
2021-05-26 20:28:37 -0400

    worked with tutor to get offline caching of data to work

--- public/indexedDb.js
+++ public/indexedDb.js
@@ -1,39 +1,51 @@
-export function useIndexedDb(databaseName, storeName, method, object) {
-    return new Promise((resolve, reject) => {
-        const request = window.indexedDB.open(databaseName, 1);
-        let db,
-            tx,
-            store;
+const request = window.indexedDB.open('budget', 1);
+let db;
 
-        request.onupgradeneeded = function (e) {
-            const db = request.result;
-            db.createObjectStore(storeName, { keyPath: "_id" });
-        };
+request.onupgradeneeded = function (e) {
+    const db = e.target.result;
+    db.createObjectStore('pending', { autoIncrement: true });
+};
 
-        request.onerror = function (e) {
-            console.log("There was an error");
-        };
+request.onerror = function (e) {
+    console.log("There was an error");
+};
 
-        request.onsuccess = function (e) {
-            db = request.result;
-            tx = db.transaction(storeName, "readwrite");
-            store = tx.objectStore(storeName);
+request.onsuccess = function (e) {
+    db = e.target.result;
+    if (navigator.onLine) {
+        checkDb();
+    }
+};
 
-            db.onerror = function (e) {
-                console.log("error");
-            };
-            if (method === "put") {
-                store.put(object);
-            }
-            if (method === "get") {
-                const all = store.getAll();
-                all.onsuccess = function () {
-                    resolve(all.result);
-                };
-            }
-            tx.oncomplete = function () {
-                db.close();
-            };
-        };
-    });
+function checkDb() {
+   let tx = db.transaction('pending', 'readwrite');
+    let store = tx.objectStore('pending');
+    let getAll = store.getAll();
+    getAll.onsuccess = function () {
+        if (getAll.result.length > 0) {
+            fetch ('/api/transaction/bulk', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+                })
+                .then(function(res) {
+                    return res.json();
+                })
+                .then(function(data) {
+                    let transaction = db.transaction('pending', 'readwrite');
+                    let storedb = transaction.objectStore('pending');
+                    storedb.clear();
+                })
+        }
+    }
 }
+
+function saveRecord(record) {
+    let transaction = db.transaction('pending', 'readwrite');
+                    let storedb = transaction.objectStore('pending');
+                    storedb.add(record);
+}
+
+window.addEventListener('online', checkDb);
